a2105045fc86627ea9e486d15cf861929ac1640d
Matthias Mueller <matthiasm@live.de>
2017-05-26 21:56:01 +0200

    Release

---
+++ Scripts/Internals/Build_Release.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MsBuild.exe" "C:\MyGit\Personal\MobileLearningSystem\MobileLearningSystem.Backend\MLS.sln" /t:Build /p:Configuration=Release /p:TargetFramework=netcoreapp1.1
---
+++ Scripts/Internals/CommitAndPush_Git.ps1
@@ -0,0 +1,4 @@
+cd '..\..\Mmu.Mls.WebServices\bin\Release';
+git add -A;
+git commit -am "Release";
+git push;
